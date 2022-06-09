eae8f3914ca6445d41e07bc1e6bed7244e7cb658
Annie Giang <annieg4123@gmail.com>
2022-06-08 23:23:24 -0700

    BaseController

---
+++ src/server/controllers/BaseController.ts
@@ -0,0 +1,44 @@
+import api from "Config/api";
+
+export default class BaseController {
+  accessToken?: string;
+  userId?: string;
+  host: string;
+  hostDomain: string;
+  apiVersion: string;
+  os: string;
+  userAgent: string;
+
+  constructor() {
+    this.accessToken = api.accessToken;
+    this.userId = api.userId;
+    this.host = api.host;
+    this.hostDomain = api.hostDomain;
+    this.apiVersion = api.apiVersion;
+    this.os = api.os;
+    this.userAgent = api.userAgent;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.accessToken && !!this.userId;
+  }
+
+  get baseApi(): string {
+    return `https://${this.host}/${this.hostDomain}/${this.apiVersion}`;
+  }
+
+  get defaultHeaders() {
+    return {
+      Host: this.host,
+      os: this.os,
+      userId: this.userId,
+      "User-Agent": this.userAgent,
+      accesstoken: this.accessToken,
+      Accept: "*/*",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br"
+    };
+  }
+}
