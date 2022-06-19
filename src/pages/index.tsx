c643adbad82b22f715e135862a274871c307386f
Annie Giang <annieg4123@gmail.com>
2022-06-19 10:39:33 -0700

    Remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import UserController from "Controllers/UserController";
-import GroupController from "Controllers/GroupController";
 import {UserInfoResponse, UserInfo} from "Types/user";
-import {GroupResponse, Group} from "Types/groups";
-import {LOONA_GROUP_ID} from "Constants/loona";
-import Members from "Components/Members";
+import {Group} from "Types/groups";
 import NavBar from "Components/navbar/NavBar";
 
 type Props = {
@@ -12,26 +9,17 @@ type Props = {
 };
 
 export default ({loona, user}: Props) => {
-  // const {enName, name, artistUsers} = loona;
-
   return (
     <div>
       <NavBar user={user} />
-      {/* <Members members={artistUsers} /> */}
     </div>
   );
 };
 
 export const getServerSideProps = async () => {
-  // const loonaResponse: GroupResponse = await GroupController.getGroupInfo(
-  //   LOONA_GROUP_ID
-  // );
-
   const userResponse: UserInfoResponse = await UserController.getInfo();
-
   return {
     props: {
-      // loona: loonaResponse.group,
       user: userResponse.user
     }
   };
