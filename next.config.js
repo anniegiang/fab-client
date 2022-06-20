395ac1d63fcd758b78b595ba688951169479002e
Annie Giang <annieg4123@gmail.com>
2022-06-19 20:52:55 -0700

    Handle server side modules in next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@
 module.exports = {
   images: {
     domains: ["dnkvjm1f8biz3.cloudfront.net"]
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      stream: false,
+      constants: false
+    };
+    return config;
   }
 };
