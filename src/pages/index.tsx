f611d10aa7243a75ccf3336e2a995c419eacb8a8
Annie Giang <annieg4123@gmail.com>
2022-06-26 12:07:59 -0700

    Add auth headers to index requests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import axios from "axios";
-import {useState, FormEvent} from "react";
+import {withSessionSsr} from "Config/withSession";
 import UserController from "Controllers/UserController";
 import Artists from "Components/Artists";
 import Groups from "Components/Groups";
 import {ArtistResponse, ArtistUser} from "Types/artist";
 import {SubscribedGroupsResponse, Group} from "Types/group";
-import {useAccessToken, useUserId} from "Client/hooks/useLocalSession";
-import LoginForm from "Client/components/LoginForm";
-import {LoginFields} from "Types/session";
-import LocalSession from "Client/LocalSession";
 
 type Props = {
   subscribedArtists: ArtistUser[];
@@ -19,24 +14,28 @@ export default ({subscribedArtists, subscribedGroups}: Props) => {
   return (
     <div>
       <h1>Subscribed Artists</h1>
-      {/* <Artists artistUsers={subscribedArtists} /> */}
+      <Artists artistUsers={subscribedArtists} />
       <h1>Subscribed Groups</h1>
-      {/* <Groups groups={subscribedGroups} /> */}
+      <Groups groups={subscribedGroups} />
     </div>
   );
 };
 
-export const getServerSideProps = async () => {
-  // const subscribedGroupsReponse: SubscribedGroupsResponse =
-  //   await UserController.getSubscribedGroups();
-  // const subscribedArtistsReponse: ArtistResponse =
-  //   await UserController.getSubscribedArtists();
+export const getServerSideProps = withSessionSsr(
+  async function getServerSideProps({req}) {
+    const {authHeaders} = req.session;
 
-  return {
-    props: {
-      //   subscribedArtists: subscribedArtistsReponse.artistUsers,
-      //   subscribedGroups: subscribedGroupsReponse.groups
-      // }
-    }
-  };
-};
+    const subscribedGroupsReponse: SubscribedGroupsResponse =
+      await UserController.getSubscribedGroups(authHeaders);
+
+    const subscribedArtistsReponse: ArtistResponse =
+      await UserController.getSubscribedArtists(authHeaders);
+
+    return {
+      props: {
+        subscribedArtists: subscribedArtistsReponse.artistUsers,
+        subscribedGroups: subscribedGroupsReponse.groups
+      }
+    };
+  }
+);
