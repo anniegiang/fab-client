bfd4d0c682136617cdfe6d9dff18cfc4990f6581
Annie Giang <annieg4123@gmail.com>
2022-07-02 12:33:01 -0700

    BaseController test

---
+++ src/server/controllers/__tests__/BaseController.spec.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import BaseController from "server/controllers/BaseController";
+
+const host = "vip-fab-api.myfab.tv";
+const hostDomain = "fapi";
+const apiVersion = "2";
+const os = "ios";
+const userAgent = "fab|ios|appstore|1.2.0|15.5|iPhone11,8|apple|en|US";
+
+const baseApi = `https://${host}/${hostDomain}/${apiVersion}`;
+
+const defaultHeaders = {
+  Host: host,
+  os,
+  "User-Agent": userAgent,
+  Accept: "*/*",
+  Connection: "keep-alive",
+  "Cache-Control": "no-cache",
+  "Accept-Language": "en-US,en;q=0.9",
+  "Accept-Encoding": "gzip, deflate, br"
+};
+
+describe("BaseController", () => {
+  const instance = new BaseController();
+
+  test("baseApi returns correctly", () => {
+    expect(instance.baseApi).toBe(baseApi);
+  });
+
+  test("defaultHeaders returns correctly", () => {
+    expect(instance.defaultHeaders).toStrictEqual(defaultHeaders);
+  });
+
+  test("api - initializes a http client", () => {
+    const spy = jest.spyOn(axios, "create");
+
+    expect(typeof instance.api).toBe("function");
+    expect(spy).toHaveBeenNthCalledWith(1, {
+      headers: defaultHeaders,
+      baseURL: baseApi,
+      timeout: 2000
+    });
+  });
+});
