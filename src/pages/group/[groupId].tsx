9f557b90f59ec76cfae7089cc3dc5d8315cc890c
Annie Giang <annieg4123@gmail.com>
2022-07-01 22:55:12 -0700

    Show group messages

--- src/pages/group/[groupId].tsx
+++ src/pages/group/[groupId].tsx
@@ -1,32 +1,53 @@
 import GroupController from "Controllers/GroupController";
 import {GroupResponse, Group} from "Types/group";
+import {GroupMessageResponse, Message} from "Types/message";
 import Artists from "Components/Artists";
+import Messages from "Components/Messages";
 import {Id} from "Types/common";
+import {withSessionSsr} from "Config/withSession";
 
 type Props = {
   group: Group;
+  groupMessages: Message[];
 };
 
-export default ({group}: Props) => {
+type ServerSideParams = {
+  groupId: Id;
+};
+
+export default ({group, groupMessages}: Props) => {
   const {artistUsers, name} = group;
 
   return (
     <div>
-      <h1>{name}</h1>
-      <Artists artistUsers={artistUsers} />
+      <section>
+        <h1>{name}</h1>
+        <Artists artistUsers={artistUsers} />
+      </section>
+      <section>
+        <h1>Messages</h1>
+        <Messages messages={groupMessages} />
+      </section>
     </div>
   );
 };
 
-export const getServerSideProps = async (context: {params: {groupId: Id}}) => {
-  const {groupId} = context.params;
+export const getServerSideProps = withSessionSsr<Props>(async (context) => {
+  const {authHeaders} = context.req.session;
+  const {groupId} = context.params as unknown as ServerSideParams;
+
   const groupResponse: GroupResponse = await GroupController.getGroupInfo(
-    groupId
+    groupId,
+    authHeaders
   );
+
+  const groupMessagesResponse: GroupMessageResponse =
+    await GroupController.getGroupMessages(groupId, authHeaders);
 
   return {
     props: {
-      group: groupResponse.group
+      group: groupResponse.group,
+      groupMessages: groupMessagesResponse.messages
     }
   };
-};
+});
