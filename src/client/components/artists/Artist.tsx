e86d761f3a514b3fdf999448402041fbc8ca0c41
Annie Giang <annieg4123@gmail.com>
2022-07-04 09:57:13 -0700

    UI to follow & unfollow artist

--- src/client/components/artists/Artist.tsx
+++ src/client/components/artists/Artist.tsx
@@ -1,6 +1,9 @@
+import axios from "axios";
+import {MouseEventHandler, useState} from "react";
 import Card from "client/components/base/Card";
 import {ArtistUser} from "types/artist";
-import artistStyles from "client/styles/Artists.module.css";
+import styles from "client/styles/Artists.module.css";
+import {yesNo} from "constants/common";
 
 type Props = {
   artistUser: ArtistUser;
@@ -12,10 +15,25 @@ const cardStyles = {
 };
 
 export default ({artistUser}: Props) => {
-  const {id, profileImage, artist} = artistUser;
+  const {id, profileImage, artist, isFollow} = artistUser;
   const {affectionateName, name, enName, statusMessage} = artist;
+  const [isFollowing, setIsFollowing] = useState(isFollow === yesNo.yes);
 
   const _name = affectionateName ? affectionateName : `${name} * ${enName}`;
+
+  const handleSubscribe: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/followArtist", {artistUserId: id})
+      .then(() => setIsFollowing(true));
+  };
+
+  const handleUnsubscribe: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/unfollowArtist", {artistUserId: id})
+      .then(() => setIsFollowing(false));
+  };
 
   return (
     <Card
@@ -27,8 +45,18 @@ export default ({artistUser}: Props) => {
       imageWidth={250}
       cardContainerStyles={cardStyles}
     >
-      <h4 className={artistStyles.artistName}>{name}</h4>
-      <p className={artistStyles.artistStatus}>{statusMessage}</p>
+      <div className={styles.content}>
+        <h4 className={styles.artistName}>{name}</h4>
+        <p className={styles.artistStatus}>{statusMessage}</p>
+        <button
+          className={`${styles.button} ${
+            isFollowing ? styles.unsubscribe : styles.subscribe
+          }`}
+          onClick={isFollowing ? handleUnsubscribe : handleSubscribe}
+        >
+          {isFollowing ? "Unsubscribe" : "Subscribe"}
+        </button>
+      </div>
     </Card>
   );
 };
