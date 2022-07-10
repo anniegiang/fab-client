e24f2d46530ef2b09301fa1c726cdfb4204d459d
Annie Giang <annieg4123@gmail.com>
2022-07-10 00:59:53 -0700

    Export comments as txt and csv

--- src/pages/message/[messageId]/comments.tsx
+++ src/pages/message/[messageId]/comments.tsx
@@ -1,5 +1,6 @@
+import moment from "moment-timezone";
 import {useRouter} from "next/router";
-import {useEffect, useRef, useState} from "react";
+import {MouseEventHandler, useEffect, useRef, useState} from "react";
 import axios from "axios";
 import {Id} from "types/common";
 import {withSessionSsr} from "config/withSession";
@@ -10,6 +11,7 @@ import MessageCommentForm from "client/components/forms/MessageCommentForm";
 import {CommentsResponse, Comment} from "types/comment";
 import CommentBubble from "client/components/messages/CommentBubble";
 import {POINTS} from "constants/points";
+import {yesNo} from "constants/common";
 
 type Props = {
   comments: Comment[];
@@ -26,12 +28,71 @@ export default ({comments}: Props) => {
   const [addedComments, setAddedComments] = useState<Comment[]>([]);
   const ref = useRef<HTMLDivElement>(null);
 
+  const _comments = [...comments, ...addedComments];
+
   useEffect(() => {
     if (ref.current) {
       const offsetBottom = ref.current.offsetTop + ref.current.offsetHeight;
       window.scrollTo({top: offsetBottom});
     }
   }, []);
+
+  const handleExportCommentsTxt: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault();
+
+    const allComments = _comments.map(
+      ({isArtist, comment, createdAt, enName}) => {
+        const timestamp = moment(createdAt).format("hh:mm:ssA YYYY-MM-DD");
+        const name = isArtist === yesNo.yes ? enName : "Me";
+        return `[${name} ${timestamp}]\n${comment}\n\n`;
+      }
+    );
+
+    const element = document.createElement("a");
+    const file = new Blob(allComments, {type: "text/plain"});
+
+    element.href = URL.createObjectURL(file);
+    element.download = `message-${messageId}-comments`;
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const handleExportCommentsCSV: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault();
+    // const timestamp = moment(createdAt).format("hh:mm:ssA YYYY-MM-DD");
+    // const name = isArtist === yesNo.yes ? enName : "Me";
+    // return `[${name} ${timestamp}]\n${comment}\n\n`;
+    const commentRows = _comments.map(
+      ({isArtist, comment, createdAt, enName}) => {
+        const timestamp = moment(createdAt).format("hh:mm:ssA YYYY-MM-DD");
+        const name = isArtist === yesNo.yes ? enName : "Me";
+        return [timestamp, name, comment];
+      }
+    );
+
+    const toCSVString = commentRows
+      .map(
+        (row) =>
+          row
+            .map(String) // convert every value to String
+
+            // eslint-disable-next-line quotes
+            .map((v) => v.replaceAll('"', '""')) // escape double colons
+            .map((v) => `"${v}"`) // quote it
+            .join(",") // comma-separated
+      )
+      .join("\r\n"); // rows starting on new lines
+
+    // Create a blob
+    const blob = new Blob([toCSVString], {type: "text/csv;charset=utf-8;"});
+    const url = URL.createObjectURL(blob);
+
+    // Create a link to download it
+    const pom = document.createElement("a");
+    pom.href = url;
+    pom.setAttribute("download", `message-${messageId}-comments.csv`);
+    pom.click();
+  };
 
   const handleAddComment = (comment: string) => {
     return axios
@@ -40,8 +101,6 @@ export default ({comments}: Props) => {
         setAddedComments([...addedComments, response.data.comment])
       );
   };
-
-  const _comments = [...comments, ...addedComments];
 
   return (
     <div className={styles.container} ref={ref}>
@@ -53,6 +112,10 @@ export default ({comments}: Props) => {
         <h4 className={styles.noComments}>No comments</h4>
       )}
       <MessageCommentForm handleAddComment={handleAddComment} />
+      <section className={styles.exports}>
+        <a onClick={handleExportCommentsCSV}>Export comments (.csv)</a>
+        <a onClick={handleExportCommentsTxt}>Export comments (.txt)</a>
+      </section>
       {[
         `* Sending comments costs ${POINTS.sendComment} point`,
         "* Your comments are sent in real time",
