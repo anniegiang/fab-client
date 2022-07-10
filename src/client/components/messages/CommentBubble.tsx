e716caf13c6103e922bbbd0458fc8d73396c299e
Annie Giang <annieg4123@gmail.com>
2022-07-10 12:33:43 -0700

    Simplify

--- src/client/components/messages/CommentBubble.tsx
+++ src/client/components/messages/CommentBubble.tsx
@@ -3,6 +3,7 @@ import moment from "moment-timezone";
 import {Comment} from "types/comment";
 import {YES_NO} from "constants/common";
 import styles from "client/styles/CommentBubble.module.css";
+import {TimeValueOf} from "types/common";
 
 type Props = {
   comment: Comment;
@@ -11,7 +12,19 @@ type Props = {
   ) => unknown;
 };
 
+type _CommentProps = {
+  comment: string;
+  createdAt: TimeValueOf;
+  isLiked: boolean;
+  writtenByArtist: boolean;
+  commentStyles?: string;
+  onDelete?: (
+    e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>
+  ) => unknown;
+};
+
 const TIMESTAMP_FORMAT = "h:mm a";
+
 export default ({comment, onDelete}: Props) => {
   const {
     comment: commentText,
@@ -30,39 +43,52 @@ export default ({comment, onDelete}: Props) => {
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={`${styles.commentContainer} ${commentStyles}`}>
-          <p className={commentStyles}>{commentText}</p>
-          <p className={`${styles.commentTimestamp} ${commentStyles}`}>
-            {moment(createdAt).format(TIMESTAMP_FORMAT)}
-          </p>
-          {isLiked && !writtenByArtist && (
-            <span className={styles.likedComment}>❤️</span>
-          )}
-        </div>
-        {!writtenByArtist && (
-          <a className={styles.deleteComment} onClick={onDelete}>
-            Delete
-          </a>
-        )}
-      </div>
+      <_Comment
+        writtenByArtist={writtenByArtist}
+        isLiked={isLiked}
+        commentStyles={commentStyles}
+        onDelete={onDelete}
+        createdAt={createdAt}
+        comment={commentText}
+      />
       {hasArtistReplies &&
         subComments.map((subComment) => (
-          <div className={styles.container}>
-            <div
-              className={`${styles.commentContainer} ${styles.artistComment} ${styles.repliedComment}`}
-            >
-              <p className={`${styles.artistComment} ${styles.repliedComment}`}>
-                {subComment.comment}
-              </p>
-              <p
-                className={`${styles.commentTimestamp} ${styles.artistComment} ${styles.repliedComment}`}
-              >
-                {moment(subComment.createdAt).format(TIMESTAMP_FORMAT)}
-              </p>
-            </div>
-          </div>
+          <_Comment
+            writtenByArtist={true}
+            isLiked={false}
+            commentStyles={`${styles.artistComment} ${styles.repliedComment}`}
+            createdAt={subComment.createdAt}
+            comment={subComment.comment}
+          />
         ))}
     </>
+  );
+};
+
+const _Comment = ({
+  comment,
+  commentStyles = "",
+  createdAt,
+  writtenByArtist,
+  isLiked,
+  onDelete
+}: _CommentProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.commentContainer} ${commentStyles}`}>
+        <p className={commentStyles}>{comment}</p>
+        <p className={`${styles.commentTimestamp} ${commentStyles}`}>
+          {moment(createdAt).format(TIMESTAMP_FORMAT)}
+        </p>
+        {isLiked && !writtenByArtist && (
+          <span className={styles.likedComment}>❤️</span>
+        )}
+      </div>
+      {!writtenByArtist && onDelete && (
+        <a className={styles.deleteComment} onClick={onDelete}>
+          Delete
+        </a>
+      )}
+    </div>
   );
 };
